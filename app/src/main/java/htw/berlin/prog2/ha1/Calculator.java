d6438ab3f8fac0d1bca5d2c7a7365f62e45fddb1
alperadem15 <alper-gencer@gmx.de>
2025-04-20 22:42:32 +0200

    Teilaufgabe3 Bugfixes

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -29,12 +29,15 @@ public class Calculator {
      * @param digit Die Ziffer, deren Taste gedrückt wurde
      */
     public void pressDigitKey(int digit) {
-        if(digit > 9 || digit < 0) throw new IllegalArgumentException();
-
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
-
+        if (digit > 9 || digit < 0) throw new IllegalArgumentException();
+    
+        if ((screen.equals("0") || (!screen.equals("0.") && latestValue == Double.parseDouble(screen)))) {
+            screen = "";
+        }
+        
         screen = screen + digit;
     }
+
     
 
     /**
